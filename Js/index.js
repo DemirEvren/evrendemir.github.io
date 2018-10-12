9476016c444f699bbc643ab91b4b930fe14afdc6
DemirEvren <evren.demir09@hotmail.com>
2018-10-12 21:19:31 +0200

    Newest Version

--- Js/index.js
+++ Js/index.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-$(document).ready(function) {
-	// body...
-	
-});
-=======
 
 $(document).ready(function () {
     // the path for the css file that is going to appended has to be relative to the index file
@@ -17,7 +11,6 @@ $(document).ready(function () {
         for (var counter = 0; counter <=10; counter++){
             $('.breather2').fadeOut(1000, function () {});
             $('.breather2').fadeIn(1000, function () {});
-
             }
     });
 
@@ -46,8 +39,4 @@ $(document).ready(function () {
              $('body').load("Views/"+pagename+".html");
         });
 
-
-
-
 });
->>>>>>> 1679c3f74b1467c7e5f81ba9d14a2e07c3dcee03
